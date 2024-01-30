f59bc3f515b03a46e51e9ec4885d42be2310382e
Mason Thomas <Kandleseatwicks@gmail.com>
2024-01-30 14:31:10 -0600

    create basic react starter project, add react router

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Navbar from './components/navbar'
+
+function App() {
+  return (
+    <>
+    <Navbar/>
+      <div>
+        <h2>We are Comet Robotics,<br/> the premiere robotics club at UTDallas </h2>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/navbar.tsx
@@ -0,0 +1,19 @@
+function Navbar() {
+
+    return (
+      <>
+        <div>
+              <ul id="nav">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Projects</a></li>
+                <li><a href="#">Contact</a></li>
+                <li><a href="#">Join</a></li>
+              </ul>
+            </div>
+      </>
+    )
+  }
+  
+  export default Navbar
+  
