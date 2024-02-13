a487965504928880a6bd45f9f2eb45ff4cb42659
Jason Antwi-Appah <hey@jasonaa.me>
2024-02-13 17:56:44 -0600

    Generate sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
-
 import react from "@astrojs/react";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  site: 'https://cometrobotics.org',
+  integrations: [react(), sitemap()]
 });
