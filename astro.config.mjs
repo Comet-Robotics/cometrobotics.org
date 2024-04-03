62264227e5dca2073b792b8a897a6e66bd486a00
Mason Thomas <Kandleseatwicks@gmail.com>
2024-04-03 10:56:53 -0500

    give url

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,8 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://cometrobotics.org',
-  integrations: [react(), sitemap()]
+  integrations: [react(), sitemap()],
+  redirects: {
+    '/give' : 'https://givingday.utdallas.edu/giving-day/83844/department/83967'
+  }
 });
