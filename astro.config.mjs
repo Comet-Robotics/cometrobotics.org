5834a37aad0620467ff87418852901238919be9e
Jason Antwi-Appah <hey@jasonaa.me>
2024-06-08 07:56:36 -0500

    Format

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
-import { defineConfig } from 'astro/config';
-import react from "@astrojs/react";
+import { defineConfig } from 'astro/config'
+import react from '@astrojs/react'
 
-import sitemap from "@astrojs/sitemap";
+import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://cometrobotics.org',
   integrations: [react(), sitemap()],
   redirects: {
-    '/give' : 'https://givingday.utdallas.edu/amb/robot'
-  }
-});
+    '/give': 'https://givingday.utdallas.edu/amb/robot',
+  },
+})
--- src/data.ts
+++ src/data.ts
@@ -1,77 +1,76 @@
-type LinkType = { name: string; href: string }
+interface LinkType { name: string, href: string }
 type LinkWithIconType = LinkType & { icon: string }
-const HOME_LINK: LinkType = { name: "Home", href: "#" };
+const HOME_LINK: LinkType = { name: 'Home', href: '#' }
 
 const mainNavLinks: LinkType[] = [
   HOME_LINK,
 //   { name: "About", href: "#" },
 //   { name: "Projects", href: "#" },
 //   { name: "Contact", href: "#" },
-];
-
+]
 
 const socialLinks: LinkWithIconType[] = [
-    { name: "GitHub", href: 'https://github.com/Comet-Robotics', icon: '/socials/github.png' },
-    { name: "Instagram", href: 'https://www.instagram.com/comet_robotics_utd/', icon: '/socials/instagram.png' },
-    // { name: "Facebook", href: 'https://www.facebook.com/utdcombatrobotics', icon: '/socials/facebook.png' },
-    { name: "LinkedIn", href: 'https://www.linkedin.com/company/comet-robotics-utd/', icon: '/socials/linkedin.png' },
-    { name: "YouTube", href: 'https://www.youtube.com/@CometRoboticsUTD', icon: '/socials/youtube.png' },
+  { name: 'GitHub', href: 'https://github.com/Comet-Robotics', icon: '/socials/github.png' },
+  { name: 'Instagram', href: 'https://www.instagram.com/comet_robotics_utd/', icon: '/socials/instagram.png' },
+  // { name: "Facebook", href: 'https://www.facebook.com/utdcombatrobotics', icon: '/socials/facebook.png' },
+  { name: 'LinkedIn', href: 'https://www.linkedin.com/company/comet-robotics-utd/', icon: '/socials/linkedin.png' },
+  { name: 'YouTube', href: 'https://www.youtube.com/@CometRoboticsUTD', icon: '/socials/youtube.png' },
 ]
 
 const sponsorLinks: LinkWithIconType[] = [
-	{
-		name: 'SolidWorks',
-        href: 'https://www.solidworks.com/',
-        icon: '/sponsors/solidworks.png'
-	},
-    {
-        name: 'BetterCNC',
-        href: 'https://www.bettercnc.com/',
-        icon: '/sponsors/bettercnc.png'
-    },
-    // {
-    //     name: 'Fingertech Robotics',
-    //     href: 'https://www.fingertechrobotics.com/',
-    //     icon: '/sponsors/fingertech.png'
-    // },
-    // {
-    //     name: 'SendCutSend',
-    //     href: 'https://sendcutsend.com/',
-    //     icon: '/sponsors/sendcutsend.png'
-    // },
-    {
-        name: 'Repeat Robotics',
-        href: 'https://www.repeat-robotics.com/',
-        icon: '/sponsors/repeat-robotics.png'
-    }
+  {
+    name: 'SolidWorks',
+    href: 'https://www.solidworks.com/',
+    icon: '/sponsors/solidworks.png',
+  },
+  {
+    name: 'BetterCNC',
+    href: 'https://www.bettercnc.com/',
+    icon: '/sponsors/bettercnc.png',
+  },
+  // {
+  //     name: 'Fingertech Robotics',
+  //     href: 'https://www.fingertechrobotics.com/',
+  //     icon: '/sponsors/fingertech.png'
+  // },
+  // {
+  //     name: 'SendCutSend',
+  //     href: 'https://sendcutsend.com/',
+  //     icon: '/sponsors/sendcutsend.png'
+  // },
+  {
+    name: 'Repeat Robotics',
+    href: 'https://www.repeat-robotics.com/',
+    icon: '/sponsors/repeat-robotics.png',
+  },
 ]
 
-const clubEmail = "cometrobotics@utdallas.edu"
+const clubEmail = 'cometrobotics@utdallas.edu'
 const discordInvite = 'https://discord.gg/dTGwav3PVM'
 
 const makerspaceMap = 'https://g.page/utdesign-makerspace?share'
 
 const footerLinks: LinkType[] = [
-    {
-        name: 'Discord',
-        href: discordInvite
-    },
-    {
-        name: 'Wiki',
-        href: '#'
-    },
-    {
-        name: 'Source Code',
-        href: 'https://github.com/Comet-Robotics/cometrobotics.org'
-    },
-    {
-        name: 'SharePoint',
-        href: '#'
-    },
-    {
-        name: 'Sponsorship',
-        href: '#'
-    }
+  {
+    name: 'Discord',
+    href: discordInvite,
+  },
+  {
+    name: 'Wiki',
+    href: '#',
+  },
+  {
+    name: 'Source Code',
+    href: 'https://github.com/Comet-Robotics/cometrobotics.org',
+  },
+  {
+    name: 'SharePoint',
+    href: '#',
+  },
+  {
+    name: 'Sponsorship',
+    href: '#',
+  },
 ]
 
-export { mainNavLinks, HOME_LINK, footerLinks, socialLinks, clubEmail, sponsorLinks, discordInvite };
+export { mainNavLinks, HOME_LINK, footerLinks, socialLinks, clubEmail, sponsorLinks, discordInvite, makerspaceMap }
