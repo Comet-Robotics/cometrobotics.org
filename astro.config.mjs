da81bf975c95bc07131a951c712bb2548557930a
Jason Antwi-Appah <hey@jasonaa.me>
2024-06-09 03:33:37 +0000

    Add MDX integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from 'astro/config'
 import react from '@astrojs/react'
-
 import sitemap from '@astrojs/sitemap'
+
+import mdx from '@astrojs/mdx'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://cometrobotics.org',
-  integrations: [react(), sitemap()],
+  integrations: [react(), sitemap(), mdx()],
   redirects: {
     '/give': 'https://givingday.utdallas.edu/amb/robot',
   },
