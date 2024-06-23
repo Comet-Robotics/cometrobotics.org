1472568e3d3765e1f1d85ddab7be22dc504edc66
Jason Antwi-Appah <hey@jasonaa.me>
2024-06-22 19:27:10 -0500

    Initial setup of OG image generation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,15 @@
-import { defineConfig } from 'astro/config'
-import react from '@astrojs/react'
-import sitemap from '@astrojs/sitemap'
+import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
+import mdx from '@astrojs/mdx';
+import { astroOgImagesGenerator } from 'og-images-generator/astro';
 
-import mdx from '@astrojs/mdx'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://cometrobotics.org',
-  integrations: [react(), sitemap(), mdx()],
+  integrations: [react(), sitemap(), mdx(), astroOgImagesGenerator()],
   redirects: {
-    '/give': 'https://givingday.utdallas.edu/amb/robot',
-  },
-})
+    '/give': 'https://givingday.utdallas.edu/amb/robot'
+  }
+});
