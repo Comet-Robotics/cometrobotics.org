9649ff80517c39c21823bfacc72f743bb2af0703
Jason Antwi-Appah <hey@jasonaa.me>
2025-04-07 11:34:27 -0500

    Add new banquet links

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,13 @@ export default defineConfig({
   site: 'https://cometrobotics.org',
   integrations: [react(), sitemap(), mdx(), astroOgImagesGenerator()],
   redirects: {
+    // comets giving day 2024
     '/give': 'https://givingday.utdallas.edu/amb/robot',
-    '/banquet': 'https://lu.ma/q6d4ofxd'
+    // general student rsvp for spring 2025 banquet
+    '/banquet-s25': 'https://lu.ma/q6d4ofxd',
+    // faculty/staff rsvp for spring 2025 banquet
+    '/banquet-s25-f': 'https://lu.ma/q6d4ofxd',
+    // supporters rsvp for spring 2025 banquet
+    '/banquet-s25-s': 'https://lu.ma/q6d4ofxd',
   }
 });
