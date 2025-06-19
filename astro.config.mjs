aabe3c57762be50e22d8f237a36a5dfc6f983672
Jason Antwi-Appah <hey@jasonaa.me>
2025-06-18 22:23:03 -0500

    chore: point sponsor shortlink to R2

--- astro.config.mjs
+++ astro.config.mjs
@@ -18,6 +18,6 @@ export default defineConfig({
     '/banquet-s25-f': 'https://lu.ma/q6d4ofxd?coupon=JEW6KT',
     // supporters rsvp for spring 2025 banquet
     '/banquet-s25-s': 'https://lu.ma/q6d4ofxd?coupon=V09001',
-    '/sponsor': 'https://drive.google.com/file/d/17vwOhQ6eRM05ebUIr4ywh5CQr3cWrjHU/view?usp=sharing',
+    '/sponsor': 'https://static.cometrobotics.org/Comet%20Robotics%20Sponsorship%20Packet%20June%2018%202025.pdf',
   }
 });
